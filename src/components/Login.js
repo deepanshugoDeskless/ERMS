a588802464ff36e5a2830a9b33237355b11c18ac
souraj pal <souraj.pal@godeskless.com>
2023-10-06 15:45:47 +0530

    indentation

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,89 +1,91 @@
-import { useMutation } from '@apollo/client'
-import { padding, spacing } from '@mui/system'
-import React,{useState} from 'react'
-import { useNavigate } from 'react-router-dom'
-import { LOGIN_USER } from '../gqloperations/mutations'
+import { useMutation } from "@apollo/client";
+import { padding, spacing } from "@mui/system";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { LOGIN_USER } from "../gqloperations/mutations";
 
 export default function Login() {
-    const navigate = useNavigate()
-    const [formData,setFormData] = useState({})
-    const [SigninUser,{error,loading,data}]= useMutation(LOGIN_USER)
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({});
+  const [SigninUser, { error, loading, data }] = useMutation(LOGIN_USER);
 
-    if(loading) return <h1>Loading</h1>
-    if(data){
-        localStorage.setItem("token",data.user.token)
-        navigate('/')
-    }
-    
-    const handleChange = (e)=>{
-        setFormData({
-         ...formData,
-         [e.target.name]:e.target.value
-        })
-    
-    }
+  if (loading) return <h1>Loading</h1>;
+  if (data) {
+    localStorage.setItem("token", data.user.token);
+    navigate("/");
+  }
 
-    const handleSubmit = (e)=>{
-        e.preventDefault()
-        SigninUser({
-            variables:{
-                userSignin:formData
-            }
-        })
-    }
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-    var bgColors = { "Default": "#81b71a",
-                    "Blue": "#00B1E1",
-                    "Cyan": "#37BC9B",
-                    "Green": "#8CC152",
-                    "Red": "#E9573F",
-                    "Yellow": "#F6BB42",
-};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    SigninUser({
+      variables: {
+        userSignin: formData,
+      },
+    });
+  };
 
-    return (
-        
-        <div className="container my-container" style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            height: '80vh',
-            flexDirection: 'column',
-            padding: '50px'
-            // background: 'rgb(253,24,126)',
-            // background: 'radial-gradient(circle, rgba(253,24,126,1) 0%, rgba(46,43,100,1) 51%, rgba(255,255,255,1) 100%)'
-          }}>
-            {
-                error &&
-                <div className="red card-panel">{error.message}</div>
-            }
+  var bgColors = {
+    Default: "#81b71a",
+    Blue: "#00B1E1",
+    Cyan: "#37BC9B",
+    Green: "#8CC152",
+    Red: "#E9573F",
+    Yellow: "#F6BB42",
+  };
 
-            <h5>Enter  Login Details Below! </h5>
-            <form onSubmit={handleSubmit}>
-                <input
-                 type="email"
-                 placeholder="              Enter your EmailID"
-                 name="email"
-                 onChange={handleChange}
-                 required
-                 style={{
-                    marginTop: '30px'
-                }}
-                 />
-                <input
-                 type="password"
-                 placeholder="            Enter Your Password"
-                 name="password"
-                 onChange={handleChange}
-                 required
-                 style={{
-                    marginTop: '30px'
-                }}
-                 />
-                 <button className="btn #673ab7 deep-purple" type="submit" style={{
-                     marginLeft: '100px',
-                     marginTop: '30px'
-                 }}>Login</button>
-            </form>
-        </div>
-    )
+  return (
+    <div
+      className="container my-container"
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "80vh",
+        flexDirection: "column",
+        padding: "50px",
+      }}
+    >
+      {error && <div className="red card-panel">{error.message}</div>}
+      <h5>Enter Login Details Below! </h5>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Enter your EmailID"
+          name="email"
+          onChange={handleChange}
+          required
+          style={{
+            marginTop: "30px",
+          }}
+        />
+        <input
+          type="password"
+          placeholder="Enter Your Password"
+          name="password"
+          onChange={handleChange}
+          required
+          style={{
+            marginTop: "30px",
+          }}
+        />
+        <button
+          className="btn #673ab7 deep-purple"
+          type="submit"
+          style={{
+            marginLeft: "100px",
+            marginTop: "30px",
+          }}
+        >
+          Login
+        </button>
+      </form>
+    </div>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,28 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
 
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-} from "@apollo/client";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/',
-  cache: new InMemoryCache()
+  uri: "http://localhost:4000/",
+  cache: new InMemoryCache(),
 });
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <ApolloProvider client={client}>
-    <App />
-    </ApolloProvider>
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
