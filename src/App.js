ef93cf4d73dff8205b326c69f9cd134878525b01
Deepanshu Rana <deepanshu.rana2021@vitstudent.ac.in>
2023-10-10 10:34:29 +0530

    added private routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-import Login from './components/Login';
-import Signup from './components/Signup';
-import Profile from './components/Profile';
-import CreateQuote from './components/CreateQuote';
-import Home from './components/Home';
-import NavBar from './components/NavBar';
-import {routes } from './routes'
-import { useRoutes } from 'react-router';
-import Sidebar from './scenes/global/Sidebar';
-import Addemployee from './components/Addemployee';
+import logo from "./logo.svg";
+import "./App.css";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Profile from "./components/Profile";
+import CreateQuote from "./components/CreateQuote";
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+import { routes, publicRoutes } from "./routes";
+import { useRoutes } from "react-router";
+import Sidebar from "./scenes/global/Sidebar";
+import Addemployee from "./components/Addemployee";
 import { ColorModeContext, useMode } from "../src/theme";
 import { useState } from "react";
 
 function App() {
-  const element = useRoutes(routes)
+  const element = useRoutes(routes);
+  const publicElement = useRoutes(publicRoutes);
+  const token = localStorage.getItem("token");
   return (
-   <>
-    <NavBar></NavBar>
-    {element}
+    <>
+      {token ? (
+        <>
+          <NavBar></NavBar> {element}
+        </>
+      ) : (
+        <>
+          <NavBar></NavBar>
+          {publicElement}
+        </>
+      )}
     </>
   );
 }
