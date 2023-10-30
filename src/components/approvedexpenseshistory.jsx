9e0da1abf1caf46c56d806c8bf8fc8d7ea22bf15
souraj pal <souraj.pal@godeskless.com>
2023-10-30 22:40:39 +0530

    fetch approved reimbursements

--- src/components/approvedexpenseshistory.jsx
+++ src/components/approvedexpenseshistory.jsx
@@ -5,12 +5,18 @@ import { useQuery } from "@apollo/client";
 import { gql } from "@apollo/client";
 import { tokens } from "../../src/theme";
 import { Loader, Error } from "./loader";
-import { GET_REIMBURSEMENTS } from "../gqloperations/mutations";
+import { GET_APPROVED_REIMBURSEMENTS, GET_REIMBURSEMENTS } from "../gqloperations/mutations";
 
 const ApprovedExpensesHistory = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
-  const { loading, error, data } = useQuery(GET_REIMBURSEMENTS);
+  const { loading, error, data } = useQuery(GET_APPROVED_REIMBURSEMENTS, {
+    context: {
+      headers: {
+        Authorization: `${localStorage.getItem("token")}`,
+      },
+    },
+  });
 
   // Function to format date as "date, month"
   const formatDate = (dateString) => {
@@ -26,13 +32,13 @@ const ApprovedExpensesHistory = () => {
   const mapTypeToExpense = (type) => {
     switch (type) {
       case "ta":
-        return "Travel Expense";
+        return "Travel";
       case "aa":
-        return "Accommodation Expense";
+        return "Accommodation";
       case "fa":
-        return "Meal Expense";
+        return "Meal";
       case "pa":
-        return "Purchase Expense";
+        return "Purchase";
       default:
         return type;
     }
@@ -41,7 +47,7 @@ const ApprovedExpensesHistory = () => {
   if (loading) return <Loader />;
   if (error) return <Error />;
 
-  const formattedData = data.ireimbursements.map((reimbursement) => ({
+  const formattedData = data.approvedReimbursements.map((reimbursement) => ({
     ...reimbursement,
     fromDate: formatDate(reimbursement.fromDate),
     toDate: formatDate(reimbursement.toDate),
@@ -59,7 +65,7 @@ const ApprovedExpensesHistory = () => {
     {
       field: "type",
       headerName: "Type",
-      flex: 1.2,
+      flex: 1,
       cellClassName: "name-column--cell",
     },
     // {
@@ -189,228 +195,3 @@ const ApprovedExpensesHistory = () => {
 };
 
 export default ApprovedExpensesHistory;
-
-// import { Box, Typography, useTheme } from "@mui/material";
-// import { DataGrid } from "@mui/x-data-grid";
-// import { useMutation, useQuery } from "@apollo/client";
-// import {
-//   GET_TEAM_MEMBERS,
-//   GET_PRE_REQUESTS,
-//   UPDATE_REIMBURSEMENTS,
-// } from "../gqloperations/mutations";
-// import { tokens } from "../../src/theme";
-// import AdminPanelSettingsOutlinedIcon from "@mui/icons-material/AdminPanelSettingsOutlined";
-// import LockOpenOutlinedIcon from "@mui/icons-material/LockOpenOutlined";
-// import SecurityOutlinedIcon from "@mui/icons-material/SecurityOutlined";
-// import Header from "./Header";
-// import { ColorModeContext, useMode } from "../../src/theme";
-// import Button from "@mui/material/Button";
-// import { ThemeProvider } from "@emotion/react";
-// import { CssBaseline } from "@mui/material";
-// import { useState } from "react";
-// import { Loader, Error } from "./loader";
-
-// const ApprovedExpensesHistory = () => {
-//   const [colorMode] = useMode();
-//   const [isSidebar, setIsSidebar] = useState(true);
-//   const theme = useTheme();
-//   const colors = tokens(theme.palette.mode);
-//   const [selectionModel, setSelectionModel] = useState([]);
-
-//   const handleSelectionModelChange = (newSelection) => {
-//     console.log(
-//       "🚀 ~ file: preApproveRequests.jsx:25 ~ handleSelectionModelChange ~ newSelection:",
-//       newSelection
-//     );
-//     setSelectionModel(newSelection);
-//   };
-//   const { loading, error, data, refetch } = useQuery(GET_PRE_REQUESTS, {
-//     context: {
-//       headers: {
-//         Authorization: `${localStorage.getItem("token")}`,
-//       },
-//     },
-//   });
-
-//   const [updateReimbursements, { updateData, updateLoading, updateError }] =
-//     useMutation(UPDATE_REIMBURSEMENTS, {
-//       onCompleted: () => {
-//         refetch({
-//           context: {
-//             headers: {
-//               Authorization: `${localStorage.getItem("token")}`,
-//             },
-//           },
-//         });
-//       },
-//     });
-
-//   if (loading) return <Loader />;
-//   if (error) return <Error />;
-
-//   const handleBulkApproveSubmit = () => {
-//     updateReimbursements({
-//       context: {
-//         headers: {
-//           Authorization: `${localStorage.getItem("token")}`,
-//         },
-//       },
-//       variables: {
-//         reimbursementsUpdateInput: {
-//           ids: selectionModel,
-//           reimbursementInput: {
-//             isapproved: true,
-//           },
-//         },
-//       },
-//     });
-//   };
-
-//   const columns = [
-//     { field: "title", headerName: "Title", flex: 1 },
-//     {
-//       field: "description",
-//       headerName: "Description",
-//       flex: 0.7,
-//       cellClassName: "name-column--cell",
-//     },
-//     {
-//       field: "type",
-//       headerName: "Type",
-//       flex: 0.5,
-//       cellClassName: "name-column--cell",
-//     },
-//     {
-//       field: "by",
-//       headerName: "Requested By",
-//       flex: 1,
-//       renderCell: ({ row }) => {
-//         return (
-//           <Box
-//             width="60%"
-//             m="0 auto"
-//             p="5px"
-//             display="flex"
-//             justifyContent="center"
-//             backgroundColor={
-//               row.role === "admin"
-//                 ? colors.greenAccent[600]
-//                 : row.role === "manager"
-//                 ? colors.greenAccent[700]
-//                 : colors.greenAccent[700]
-//             }
-//             borderRadius="4px"
-//           >
-//             {row.by.firstName + " " + row.by.lastName}
-//             <Typography color={colors.grey[100]} sx={{ ml: "5px" }}>
-//               {row.role}
-//             </Typography>
-//           </Box>
-//         );
-//       },
-//     },
-
-//     {
-//       field: "visitLocation",
-//       headerName: "Place",
-//       flex: 0.5,
-//     },
-//     {
-//       field: "fromDate",
-//       headerName: "From",
-//       flex: 0.7,
-//     },
-//     {
-//       field: "toDate",
-//       headerName: "To",
-//       flex: 0.7,
-//     },
-//     {
-//       field: "askedAmount",
-//       headerName: "Ask",
-//       flex: 1,
-//     },
-//   ];
-
-//   return (
-//     <>
-//       <Box
-//         style={{
-//           position: "absolute",
-//           bottom: "0px",
-//           left: "5em",
-//           right: "0px",
-//         }}
-//       >
-//         <div
-//           style={{
-//             backgroundColor:'yellow',
-//             color: colors.blueAccent[200],
-//             width: "100%",
-//             position:'relative',
-//             zIndex: -1,
-//             flex: 1,
-//             height: "100%",
-//             display: "flex",
-//             textAlign: "center",
-//             alignItems: "center",
-//             justifyContent: "space-between",
-
-//           }}
-//           className="banner"
-//         >
-//           <h4
-//             style={{
-//               marginLeft: 20,
-//               fontFamily: "Bebas Neue,sans-serif",
-//               fontSize: "xxx-large",
-//             }}
-//           >
-//             Approved Expenses
-//           </h4>
-//         </div>
-//         <Box
-//           height="80vh"
-//           sx={{
-//             "& .MuiDataGrid-root": {
-//               border: "none",
-//             },
-//             "& .MuiDataGrid-cell": {
-//               borderBottom: "none",
-//             },
-//             "& .name-column--cell": {
-//               color: colors.greenAccent[300],
-//             },
-//             "& .MuiDataGrid-columnHeaders": {
-//               backgroundColor: colors.blueAccent[700],
-//               borderBottom: "none",
-//             },
-//             "& .MuiDataGrid-virtualScroller": {
-//               backgroundColor: colors.primary[400],
-//             },
-//             "& .MuiDataGrid-footerContainer": {
-//               borderTop: "none",
-//               backgroundColor: colors.blueAccent[700],
-//             },
-//             "& .MuiCheckbox-root": {
-//               color: `${colors.greenAccent[200]} !important`,
-//             },
-//           }}
-//         >
-//           <DataGrid
-//             checkboxSelection
-//             onRowSelectionModelChange={(newRowSelectionModel) => {
-//               handleSelectionModelChange(newRowSelectionModel);
-//             }}
-//             rowSelectionModel={selectionModel}
-//             rows={data.pendingPreRequests}
-//             columns={columns}
-//             getRowId={(row) => row._id}
-//           />
-//         </Box>
-//       </Box>
-//     </>
-//   );
-// };
-
-// export default ApprovedExpensesHistory;
