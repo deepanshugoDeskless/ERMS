535961413bd6027df72be81d2b4a49b3118398f3
souraj pal <souraj.pal@godeskless.com>
2023-10-31 10:45:25 +0530

    approval screen ui

--- src/components/approveReimbursements.jsx
+++ src/components/approveReimbursements.jsx
@@ -6,8 +6,10 @@ import TextField from "@mui/material/TextField";
 import { tokens } from "../../src/theme";
 import { useMode } from "../../src/theme";
 import { useQuery, gql, useMutation } from "@apollo/client";
-import { ADMIN_FETCH_REQUESTS, UPDATE_REIMBURSEMENTS } from "../gqloperations/mutations";
-
+import {
+  ADMIN_FETCH_REQUESTS,
+  UPDATE_REIMBURSEMENTS,
+} from "../gqloperations/mutations";
 
 const getTypeDescription = (type) => {
   switch (type) {
@@ -23,7 +25,6 @@ const getTypeDescription = (type) => {
       return type;
   }
 };
-
 
 const PreApproveRequest = (key, showPlusButton, addForm) => {
   const [isFormOpen, setFormOpen] = useState(false);
@@ -98,58 +99,58 @@ const PreApproveRequest = (key, showPlusButton, addForm) => {
     });
   };
 
-  
-
   const months = {
-    '01': 'Jan',
-    '02': 'Feb',
-    '03': 'Mar',
-    '04': 'Apr',
-    '05': 'May',
-    '06': 'Jun',
-    '07': 'Jul',
-    '08': 'Aug',
-    '09': 'Sep',
-    '10': 'Oct',
-    '11': 'Nov',
-    '12': 'Dec',
+    "01": "Jan",
+    "02": "Feb",
+    "03": "Mar",
+    "04": "Apr",
+    "05": "May",
+    "06": "Jun",
+    "07": "Jul",
+    "08": "Aug",
+    "09": "Sep",
+    10: "Oct",
+    11: "Nov",
+    12: "Dec",
   };
-  
-  const reimbursements = data.pendingReimbursements.map((reimbursement, index) => {
-    // Split the date strings into day, month, and year
-    const fromDateStringParts = reimbursement.fromDate.split('/');
-    const toDateStringParts = reimbursement.toDate.split('/');
-  
-    // Ensure the date strings are in the format "DD/MM/YYYY"
-    if (fromDateStringParts.length === 3 && toDateStringParts.length === 3) {
-      const fromDay = fromDateStringParts[0];
-      const fromMonth = months[fromDateStringParts[1]];
-      const toDay = toDateStringParts[0];
-      const toMonth = months[toDateStringParts[1]];
-  
-      return {
-        id: reimbursement._id,
-        title: reimbursement.title,
-        fromDate: `${fromDay} ${fromMonth}`,
-        toDate: `${toDay} ${toMonth}`,
-        type: getTypeDescription(reimbursement.type),
-        askedAmount: reimbursement.askedAmount,
-        expenses: reimbursement.expenses,
-        description: reimbursement.description,
-      };
-    } else {
-      // Handle invalid date strings here
-      console.error('Invalid date format:', reimbursement.fromDate, reimbursement.toDate);
+
+  const reimbursements = data.pendingReimbursements.map(
+    (reimbursement, index) => {
+      // Split the date strings into day, month, and year
+      const fromDateStringParts = reimbursement.fromDate.split("/");
+      const toDateStringParts = reimbursement.toDate.split("/");
+
+      // Ensure the date strings are in the format "DD/MM/YYYY"
+      if (fromDateStringParts.length === 3 && toDateStringParts.length === 3) {
+        const fromDay = fromDateStringParts[0];
+        const fromMonth = months[fromDateStringParts[1]];
+        const toDay = toDateStringParts[0];
+        const toMonth = months[toDateStringParts[1]];
+
+        return {
+          id: reimbursement._id,
+          title: reimbursement.title,
+          fromDate: `${fromDay} ${fromMonth}`,
+          toDate: `${toDay} ${toMonth}`,
+          type: getTypeDescription(reimbursement.type),
+          askedAmount: reimbursement.askedAmount,
+          expenses: reimbursement.expenses,
+          description: reimbursement.description,
+        };
+      } else {
+        // Handle invalid date strings here
+        console.error(
+          "Invalid date format:",
+          reimbursement.fromDate,
+          reimbursement.toDate
+        );
+      }
     }
-  });
-  
-  
-  
-  
+  );
 
   const columns = [
-    { field: "title", headerName: "Title", flex: 1.4},
-    { field: "type", headerName: "Type", flex: 1.6},
+    { field: "title", headerName: "Title", flex: 1.4 },
+    { field: "type", headerName: "Type", flex: 1.6 },
     { field: "fromDate", headerName: "From Date", flex: 0.8 },
     { field: "toDate", headerName: "To Date", flex: 0.8 },
     { field: "askedAmount", headerName: "Ask", flex: 0.7 },
@@ -167,7 +168,6 @@ const PreApproveRequest = (key, showPlusButton, addForm) => {
       >
         <div
           style={{
-            // backgroundColor: "yellow",
             color: colors.blueAccent[200],
             width: "100%",
             top: "0px",
@@ -206,236 +206,248 @@ const PreApproveRequest = (key, showPlusButton, addForm) => {
             Approve
           </Button>
         </div>
-        <Box
-          height="82.3vh"
-          width="40vw"
-          sx={{
-            "& .MuiDataGrid-root": {
-              border: "none",
-            },
-            "& .MuiDataGrid-cell": {
-              borderBottom: "none",
-            },
-            "& .name-column--cell": {
-              color: colors.greenAccent[300],
-            },
-            "& .MuiDataGrid-columnHeaders": {
-              backgroundColor: colors.blueAccent[700],
-              borderBottom: "none",
-            },
-            "& .MuiDataGrid-virtualScroller": {
-              backgroundColor: colors.primary[400],
-            },
-            "& .MuiDataGrid-footerContainer": {
-              borderTop: "none",
-              backgroundColor: colors.blueAccent[700],
-            },
-            "& .MuiCheckbox-root": {
-              color: `${colors.greenAccent[200]} !important`,
-            },
-          }}
-        >
-          <DataGrid
-            checkboxSelection
-            onRowSelectionModelChange={(newRowSelectionModel) => {
-              handleSelectionModelChange(newRowSelectionModel);
-            }}
-            rowSelectionModel={selectionModel}
-            rows={reimbursements}
-            columns={columns}
-            getRowId={(row) => row.id}
-          />
-        </Box>
-      </Box>
 
-      {isFormOpen && selectedRowData && (
         <div
           style={{
-            position: "relative",
-            marginTop: "0.9em",
-            marginLeft: "7.6em",
-            width: "calc(70% - 7.5em)",
-            backgroundColor: "white",
-            maxHeight: "82vh",
-            overflowY: "auto",
-            borderLeft: "2px solid #ccc",
-            boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.2)",
-            paddingLeft:'0.2em',
-            paddingRight:'0.4em'
+            bottom: "0px",
+            left: "5em",
+            flexDirection: "row",
+            display: "flex",
+            right: "0px",
           }}
         >
-          <div
-            style={{
-              position: "sticky",
-              top: "0",
-              backgroundColor: "white",
-              zIndex: 1,
+          <Box
+            height="82.3vh"
+            width="40vw"
+            sx={{
+              "& .MuiDataGrid-root": {
+                border: "none",
+              },
+              "& .MuiDataGrid-cell": {
+                borderBottom: "none",
+              },
+              "& .name-column--cell": {
+                color: colors.greenAccent[300],
+              },
+              "& .MuiDataGrid-columnHeaders": {
+                backgroundColor: colors.blueAccent[700],
+                borderBottom: "none",
+              },
+              "& .MuiDataGrid-virtualScroller": {
+                backgroundColor: colors.primary[400],
+              },
+              "& .MuiDataGrid-footerContainer": {
+                borderTop: "none",
+                backgroundColor: colors.blueAccent[700],
+              },
+              "& .MuiCheckbox-root": {
+                color: `${colors.greenAccent[200]} !important`,
+              },
             }}
           >
-            <h4
+            <DataGrid
+              checkboxSelection
+              onRowSelectionModelChange={(newRowSelectionModel) => {
+                handleSelectionModelChange(newRowSelectionModel);
+              }}
+              rowSelectionModel={selectionModel}
+              rows={reimbursements}
+              columns={columns}
+              getRowId={(row) => row.id}
+            />
+          </Box>
+
+          {isFormOpen && selectedRowData && (
+            <div
               style={{
-                marginBottom: "0.5em",
-                fontSize: "x-large",
-                fontSize: 50,
-                fontWeight: 400,
-                position:'relative',
-                left:'-3.2em',
-                color: colors.blueAccent[200],
+                backgroundColor: "white",
+                maxHeight: "82vh",
+                overflowY: "auto",
+                width: "100%",
+                marginLeft: 16,
+                marginRight: 16,
+                paddingBottom: 16,
+                // borderLeft: "2px solid #ccc",
+                // boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.2)",
+                right: "0px",
               }}
             >
-              {selectedRowData.description}
-            </h4>
-            <Typography
-              style={{
-                fontSize: "xx-large",
-                marginTop: "-2.3em",
-                marginLeft: "6.4em",
-                color: "#808080",
-              }}
-            >
-              Amount: ₹{calculateTotalAmount(selectedRowData.expenses)}
-            </Typography>
-          </div>
-          {selectedRowData.expenses.map((expense, index) => (
-  <div
-    key={index}
-    style={{
-      border: "1px solid #ccc",
-      boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
-      padding: "0.5em",
-      borderRadius: "12px",
-      marginTop: "0.4em",
-      // backgroundColor: 'yellow',
-      display: 'flex',
-      flexDirection: 'column'
-    }}
-  >
-    <h6
-      style={{
-        marginTop: index === 0 ? "-0.5em" : "0.2em",
-        position:'relative',
-        left:'-7.4em',
-        marginBottom: "1em",
-        fontSize: "0.6em",
-        fontWeight:'500'
-      }}
-    >
-      {expense.description}
-    </h6>
-    <div
-      className="innerbox"
-      style={{ display: "flex", justifyContent: "space-evenly" }}
-    >
-      <Box
-        component="form"
-        sx={{
-          "& .MuiTextField-root": { m: 1, width: "3ch" },
-        }}
-        noValidate
-        autoComplete="off"
-        style={{ position: "relative", marginTop: "-0.16em" }}
-      >
-        <TextField
-          id="standard-read-only-input"
-          label="Amount"
-          defaultValue={`₹${expense.amount}`}
-          InputProps={{
-            readOnly: true,
-          }}
-          variant="standard"
-          style={{ width: "2.4ch" }}
-        />
-      </Box>
-      <Box
-        component="form"
-        sx={{
-          "& .MuiTextField-root": { m: 1, width: "10ch" },
-        }}
-        noValidate
-        autoComplete="off"
-        style={{ position: "relative", marginTop: "-0.16em" }}
-      >
-        <TextField
-          id="invoiceId"
-          label="Invoice ID"
-          defaultValue={expense.invoiceId}
-          variant="standard"
-          style={{ width: "3ch" }}
-          InputProps={{
-            readOnly: true,
-          }}
-        />
-      </Box>
-      <Box
-        component="form"
-        sx={{
-          "& .MuiTextField-root": { m: 1, width: "10ch" },
-        }}
-        noValidate
-        autoComplete="off"
-        style={{ position: "relative", marginTop: "-0.16em" }}
-      >
-        <TextField
-          id="establishment"
-          label="Establishment"
-          defaultValue={expense.establishment}
-          variant="standard"
-          style={{ width: "3ch" }}
-          InputProps={{
-            readOnly: true,
-          }}
-        />
-      </Box>
-      <Box
-        component="form"
-        sx={{
-          "& .MuiTextField-root": { m: 1, width: "10ch" },
-        }}
-        noValidate
-        autoComplete="off"
-        style={{ position: "relative", marginTop: "-0.16em" }}
-      >
-        <TextField
-          id="Type"
-          label="Type"
-          defaultValue={
-            expense.type === 'fe' ? 'Meal Expense' :
-            expense.type === 'ae' ? 'Accommodation Expense' :
-            expense.type === 'pe' ? 'Purchase Expense' :
-            expense.type === 'te' ? 'Travel Expense' : expense.type
-          }
-          variant="standard"
-          style={{ width: "6.4ch" }}
-          InputProps={{
-            readOnly: true,
-          }}
-        />
-      </Box>
-      <Box
-        component="form"
-        sx={{
-          "& .MuiTextField-root": { m: 1, width: "10ch" },
-        }}
-        noValidate
-        autoComplete="off"
-        style={{ position: "relative", marginTop: "-0.16em" }}
-      >
-        <TextField
-          id="Date"
-          label="Date"
-          defaultValue={expense.date}
-          variant="standard"
-          style={{ width: "3ch" }}
-          InputProps={{
-            readOnly: true,
-          }}
-        />
-      </Box>
-    </div>
-  </div>
-))}
+              <div
+                style={{
+                  top: "0",
+                  backgroundColor: "white",
+                  flexDirection: "row",
+                  display: "flex",
+                  margin: 16,
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                }}
+              >
+                <h4
+                  style={{
+                    marginBottom: "0.5em",
+                    fontSize: "x-large",
+                    fontSize: 50,
+                    fontWeight: 400,
+                    color: colors.blueAccent[200],
+                  }}
+                >
+                  {selectedRowData.description}
+                </h4>
+                <Typography
+                  style={{
+                    fontSize: "xx-large",
+                    color: "#808080",
+                  }}
+                >
+                  Amount: ₹{calculateTotalAmount(selectedRowData.expenses)}
+                </Typography>
+              </div>
+              {selectedRowData.expenses.map((expense, index) => (
+                <div
+                  key={index}
+                  style={{
+                    border: "1px solid #ccc",
+                    boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
+                    borderRadius: "12px",
+                    marginTop: "0.4em",
+                    // backgroundColor: 'yellow',
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "self-start"
+                  }}
+                >
+                  <h6
+                    style={{
+                      padding: 8,
+                      marginBottom: "1em",
+                      fontSize: "0.4em",
+                      fontWeight: "400",
+                    }}
+                  >
+                    {expense.description}
+                  </h6>
+                  <div
+                    className="innerbox"
+                    style={{ display: "flex", justifyContent: "space-evenly" }}
+                  >
+                    <Box
+                      component="form"
+                      sx={{
+                        "& .MuiTextField-root": { m: 1, width: "3ch" },
+                      }}
+                      noValidate
+                      autoComplete="off"
+                      style={{ position: "relative", marginTop: "-0.16em" }}
+                    >
+                      <TextField
+                        id="standard-read-only-input"
+                        label="Amount"
+                        defaultValue={`₹${expense.amount}`}
+                        InputProps={{
+                          readOnly: true,
+                        }}
+                        variant="standard"
+                        style={{ width: "2.4ch" }}
+                      />
+                    </Box>
+                    <Box
+                      component="form"
+                      sx={{
+                        "& .MuiTextField-root": { m: 1, width: "10ch" },
+                      }}
+                      noValidate
+                      autoComplete="off"
+                      style={{ position: "relative", marginTop: "-0.16em" }}
+                    >
+                      <TextField
+                        id="invoiceId"
+                        label="Invoice ID"
+                        defaultValue={expense.invoiceId}
+                        variant="standard"
+                        style={{ width: "3ch" }}
+                        InputProps={{
+                          readOnly: true,
+                        }}
+                      />
+                    </Box>
+                    <Box
+                      component="form"
+                      sx={{
+                        "& .MuiTextField-root": { m: 1, width: "10ch" },
+                      }}
+                      noValidate
+                      autoComplete="off"
+                      style={{ position: "relative", marginTop: "-0.16em" }}
+                    >
+                      <TextField
+                        id="establishment"
+                        label="Establishment"
+                        defaultValue={expense.establishment}
+                        variant="standard"
+                        style={{ width: "3ch" }}
+                        InputProps={{
+                          readOnly: true,
+                        }}
+                      />
+                    </Box>
+                    <Box
+                      component="form"
+                      sx={{
+                        "& .MuiTextField-root": { m: 1, width: "10ch" },
+                      }}
+                      noValidate
+                      autoComplete="off"
+                      style={{ position: "relative", marginTop: "-0.16em" }}
+                    >
+                      <TextField
+                        id="Type"
+                        label="Type"
+                        defaultValue={
+                          expense.type === "fe"
+                            ? "Meal Expense"
+                            : expense.type === "ae"
+                            ? "Accommodation Expense"
+                            : expense.type === "pe"
+                            ? "Purchase Expense"
+                            : expense.type === "te"
+                            ? "Travel Expense"
+                            : expense.type
+                        }
+                        variant="standard"
+                        style={{ width: "6.4ch" }}
+                        InputProps={{
+                          readOnly: true,
+                        }}
+                      />
+                    </Box>
+                    <Box
+                      component="form"
+                      sx={{
+                        "& .MuiTextField-root": { m: 1, width: "10ch" },
+                      }}
+                      noValidate
+                      autoComplete="off"
+                      style={{ position: "relative", marginTop: "-0.16em" }}
+                    >
+                      <TextField
+                        id="Date"
+                        label="Date"
+                        defaultValue={expense.date}
+                        variant="standard"
+                        style={{ width: "3ch" }}
+                        InputProps={{
+                          readOnly: true,
+                        }}
+                      />
+                    </Box>
+                  </div>
+                </div>
+              ))}
+            </div>
+          )}
         </div>
-      )}
+      </Box>
     </>
   );
 };
