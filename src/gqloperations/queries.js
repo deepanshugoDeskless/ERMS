bd2dc62e84d5ee5a7c4ae89c44305bb2f857237f
souraj pal <souraj.pal@godeskless.com>
2023-10-31 10:29:55 +0530

    removed code

--- src/gqloperations/queries.js
+++ src/gqloperations/queries.js
@@ -1,12 +1,1 @@
 import {gql} from '@apollo/client'
-export const GET_ALL_QUOTES = gql`
-query getAllQuotes{
-    quotes{
-      name
-      by{
-        _id
-        firstName
-      }
-    }
-  }
-`
